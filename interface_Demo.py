b306ef27172f272317c450948381e49f4e3e0f00
Shuailin Xue <xueshuailin@163.com>
2026-01-31 21:25:31 +0800

    Create interface_Demo.py

---
+++ interface_Demo.py
@@ -0,0 +1,27 @@
+from utils import load_config
+from data import run_visium_hd_preprocessing
+from model import run_get_hr
+from model import run_feature_extraction
+from pipeline import run_main
+import argparse
+
+def main():
+    config = load_config('configs/config_demo.yaml')
+
+    run_feature_extraction(config)
+
+    run_main(config)
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Main script bridging data preprocessing and model training.")
+
+    parser.add_argument('--config',
+                        type=str,
+                        default='configs/config_demo.yaml',
+                        help='Path to the YAML configuration file')
+
+    args = parser.parse_args()
+
+    main(args.config)
